0295b6a6fd408d2a1118d017d527ac9ec8bca452
1vel <95609553+1vel@users.noreply.github.com>
2022-08-14 02:33:50 +0200

    70 size

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,12 @@ loadPage = (page) => {
 }
 
 toggle = () => {
-    let state = document.getElementById('absol__menu').style
+    let state = document.getElementById('absol__menus').style
     if (state.visibility === 'visible') {
-      document.getElementById('absol__menu--toggle').src = 'assets/icon/menu.svg'
+      document.getElementById('absol__menu--toggles').src = 'assets/icon/menu.svg'
       state.visibility = 'hidden'
     } else {
-      document.getElementById('absol__menu--toggle').src = 'assets/icon/closemenu.svg'
+      document.getElementById('absol__menu--toggles').src = 'assets/icon/closemenu.svg'
       state.visibility = 'visible'
     }
 }
